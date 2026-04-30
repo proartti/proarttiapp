82a2ea4f56cc73cc78b068830089301281d5f3a3
Tiago Donatti <tdonatti777@gmail.com>
2026-04-30 00:29:21 -0700

    fix(routes): add api.v1. name prefix to prevent collision with web route names

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\V1\ClientController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->name('api.v1.')->group(function () {
     Route::apiResource('clients', ClientController::class);
 });
